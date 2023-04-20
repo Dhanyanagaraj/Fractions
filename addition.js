ea1e13b4a914f60f235b405c0daaa14e56e2ffb4
srivijayalakshmi.AT <114483681+21viji@users.noreply.github.com>
2023-04-20 12:42:08 +0530

    Update addition.js

--- addition.js
+++ addition.js
@@ -21,35 +21,3 @@ console.log(calc.getResult());
 
 
 
-//subraction(viji)
-class Fraction {
-  constructor(numerator, denominator) {
-    this.numerator = numerator;
-    this.denominator = denominator;
-  }
-  
-  subtract(otherFraction) {
-    // Find a common denominator
-    const commonDenominator = this.denominator * otherFraction.denominator;
-    
-    // Convert both fractions to the common denominator
-    const thisNumerator = this.numerator * otherFraction.denominator;
-    const otherNumerator = otherFraction.numerator * this.denominator;
-    
-    // Subtract the numerators
-    const resultNumerator = thisNumerator - otherNumerator;
-    
-    // Create and return the resulting fraction
-    return new Fraction(resultNumerator, commonDenominator);
-  }
-  
-  toString() {
-    return `${this.numerator}/${this.denominator}`;
-  }
-}
-
-// Example usage
-const fraction1 = new Fraction(3, 4);
-const fraction2 = new Fraction(1, 2);
-const result = fraction1.subtract(fraction2);
-console.log(result.toString()); 
